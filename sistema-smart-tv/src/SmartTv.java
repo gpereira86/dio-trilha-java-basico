dec7f6014e73d1d2788728eaa6abb65f5dd72025
Glauco Pereira <glaucoadm@gmail.com>
2024-10-18 15:09:30 -0300

    métodos

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,37 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;  
+    
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }    
+    
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando volume para: " + volume);
+    }
+
+    public void diminuitVolume(){
+        volume--;
+        System.out.println("Diminuindo volume para: " + volume);
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+
+}
