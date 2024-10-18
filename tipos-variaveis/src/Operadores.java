7d4d43f5719a64e13408c534cc4e199339a84333
Glauco Pereira <glaucoadm@gmail.com>
2024-10-18 14:41:15 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,121 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        
+        if (condicao1 || condicao2){
+            System.out.println("Uma das duas condições é verdadeiraa");
+        }
+
+        System.out.println("Fim!");
+        
+        
+        
+        // String nomeUm = "GLAUCO";       
+        // String nomeDois = new String("GLAUCO");       
+        
+        // System.out.println(nomeUm == nomeDois);
+        // System.out.println(nomeUm.equals(nomeDois));
+        
+        
+        // int numero1 = 1;
+        // int numero2 = 2;
+        
+        // boolean simNao = numero1 == numero2;
+
+        // if(numero1 < numero2) {
+        //     System.out.println("a nossa condição é verdadeira");
+        // }
+
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        // simNao = numero1 > numero2;
+
+        // System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+
+
+        
+        
+        // int a, b;
+        
+        // a = 6;
+        // b = 6;
+        
+        // String resultado = a==b ? "verdadeiro" : "falso" ;
+        // // if (a == b){
+        // //     resultado = "verdadeiro";
+        // // } else {
+        // //     resultado = "falso";
+        // // } 
+
+        // System.out.println(resultado);
+
+
+        
+        
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+
+        // System.out.println(variavel);
+
+        // variavel = !variavel;        
+        // System.out.println(variavel);
+
+
+        // int numero = 5;
+
+        // //x repetiçao
+        // //numero++;
+
+        // System.out.println(++ numero);
+
+        // System.out.println(-- numero);
+
+
+
+        // int numero = 5;
+
+        // numero = - numero;
+
+        // System.out.println(numero);
+
+        // numero = numero * -1;
+
+        // System.out.println(numero);
+  
+  
+  
+        // String concatenacao = "?";
+
+        // concatenacao = 1+1+1+"1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1+"1"+1+1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1+"1"+1+"1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+1+1+1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+(1+1+1);
+        // System.out.println(concatenacao);
+
+
+
+        // String nomeCompleto = "GLAUCO" + " " + "PEREIRA";
+        // System.out.println(nomeCompleto);
+
+    }
+}
